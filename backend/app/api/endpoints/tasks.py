43c655d15a90254a02d3be88fa88ea3f8ff31e69
meghnaik <meghnaik717@gmail.com>
2026-09-20 16:33:06 +0530

    Update tasks endpoint

--- backend/app/api/endpoints/tasks.py
+++ backend/app/api/endpoints/tasks.py
@@ -1,5 +1,5 @@
 from typing import Any, List, Optional
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy.orm import Session
 from sqlalchemy import or_
 
